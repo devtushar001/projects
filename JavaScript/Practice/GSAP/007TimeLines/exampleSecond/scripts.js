e75a0bc1482f65fc3eaef74c58a8f745f7aa2654
devtushar001 <tc341269@gmail.com>
2024-09-18 02:40:47 -0500

    Not working

---
+++ JavaScript/Practice/GSAP/007TimeLines/exampleSecond/scripts.js
@@ -0,0 +1,21 @@
+let tl = gsap.timeline({
+    default: {
+        duration: 1,
+    },
+})
+.to("h1", {
+    y: -100,
+    color: "white"
+})
+.to(".box1", {
+    x: -100,
+    backgroundColor: "cyan",
+})
+.to(".box2", {
+    y: 100,
+    backgroundColor: "purple",
+})
+.to(".box3", {
+    x: 100,
+    backgroundColor: "crimson"
+})
