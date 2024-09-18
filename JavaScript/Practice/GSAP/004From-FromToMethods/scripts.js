377855e86849cd33bd16fcd9b8eeedb4284df6f2
devtushar001 <tc341269@gmail.com>
2024-09-18 00:44:40 -0500

    from and from to methods in gsap comletetion

---
+++ JavaScript/Practice/GSAP/004From-FromToMethods/scripts.js
@@ -0,0 +1,15 @@
+gsap.from(".box1", {
+    y: -200,
+    duration: 3,
+    ease: "ease-in",
+    repeat: -1,
+    yoyo: true,
+});
+
+gsap.fromTo(".box2", {
+  y: 200, opacity: 0,
+}, {
+  opacity: 1, y: -200, duration: 3, borderRadius: 0, repeat: -1, yoyo: true,
+  
+})
+
